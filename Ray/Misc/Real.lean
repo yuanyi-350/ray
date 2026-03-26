bc8aa1aa19d3c3564412ad7f6011bd0bc19c4c1e
Yi.Yuan <kysyy1@126.com>
2026-03-26 22:49:19 +0800

    fix: Ray/Misc/Real.lean

--- Ray/Misc/Real.lean
+++ Ray/Misc/Real.lean
@@ -1,6 +1,7 @@
 module
 import Mathlib.Analysis.InnerProductSpace.Basic
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Deriv
+import Mathlib.Analysis.SpecialFunctions.Trigonometric.DerivHyp
 import Ray.Misc.Bound
 
 /-!
@@ -27,15 +28,15 @@ lemma Real.analyticAt_tanh : AnalyticAt ℝ tanh x := by
     Continuous (fun x ↦ tanh (f x)) := by
   fun_prop
 
-lemma Real.tanh_lt_one (x : ℝ) : tanh x < 1 := by
+lemma Real.tanh_lt_one' (x : ℝ) : tanh x < 1 := by
   simp only [Real.tanh_eq_sinh_div_cosh, div_lt_one (Real.cosh_pos _)]
   exact sinh_lt_cosh x
 
-lemma Real.neg_one_lt_tanh (x : ℝ) : -1 < tanh x := by
+lemma Real.neg_one_lt_tanh' (x : ℝ) : -1 < tanh x := by
   rw [neg_lt, ← Real.tanh_neg]
   apply tanh_lt_one
 
-lemma Real.abs_tanh_lt_one (x : ℝ) : |tanh x| < 1 :=
+lemma Real.abs_tanh_lt_one' (x : ℝ) : |tanh x| < 1 :=
   abs_lt.mpr ⟨Real.neg_one_lt_tanh x, Real.tanh_lt_one x⟩
 
 lemma Real.hasDerivAt_tanh {x : ℝ} : HasDerivAt tanh (1 / cosh x ^ 2) x := by
